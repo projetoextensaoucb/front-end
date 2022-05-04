788bad41b776e806b4e572aa7b5259540e0609b6
Eduardo Pontes <eduardo.pontes2801@gmail.com>
2022-05-04 11:00:28 -0300

    Setup front-end
    Primeiro setup front-end do projeto de extensão.

---
+++ projeto-extensao-front/src/Routes.js
@@ -0,0 +1,17 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+
+// import {  } from "./pages/ ";
+
+export function AppRoutes() {
+    return (
+        <Router>
+            <Routes>
+               {/* <Route path="/" element={< />} /> */}
+            </Routes>
+        </Router>
+    )
+}
