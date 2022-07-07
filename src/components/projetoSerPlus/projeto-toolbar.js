a18288332d49d97ed14cc48d35c953600d1ce171
Passini <pascinigui@gmail.com>
2022-07-06 22:51:31 -0300

    feat-update/buttons update and new pages

---
+++ src/components/projetoSerPlus/projeto-toolbar.js
@@ -0,0 +1,48 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  InputAdornment,
+  SvgIcon,
+  Typography
+} from '@mui/material';
+import { Download as DownloadIcon } from '../../icons/download';
+import { Search as SearchIcon } from '../../icons/search';
+import { Upload as UploadIcon } from '../../icons/upload';
+import NextLink from 'next/link';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+
+export const ProductListToolbar = (props) => (
+  <Box {...props}>
+    <NextLink
+      href="/products"
+      passHref
+    >
+      <Button
+        component="a"
+        startIcon={<ArrowBackIcon fontSize="small" />}
+      >
+        Retornar para tela de projetos
+      </Button>
+    </NextLink>
+    <Box
+      sx={{
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+        m: 1
+      }}
+    >       
+      {/* <Typography
+        sx={{ m: 2 }}
+        variant="h4"
+      >
+        Projeto Ser+
+      </Typography> */}
+    </Box>
+  </Box>
+);
