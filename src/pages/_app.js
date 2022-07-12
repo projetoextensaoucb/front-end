32c80b1998d429f208114ae7ee481e8693271e7e
Igor Silva <igorsilvadeveloper@gmail.com>
2022-07-12 20:33:58 -0300

    fix: remove material ui import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,5 @@
 import Head from 'next/head';
 import { CacheProvider } from '@emotion/react';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { createEmotionCache } from '../utils/create-emotion-cache';
@@ -27,12 +25,10 @@ const App = (props) => {
           content="initial-scale=1, width=device-width"
         />
       </Head>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           {getLayout(<Component {...pageProps} />)}
         </ThemeProvider>
-      </LocalizationProvider>
     </CacheProvider>
   );
 };
